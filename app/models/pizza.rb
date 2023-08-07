75cf218c8c1dc6ed36c2418d26756234dbb20e6e
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2023-08-06 17:33:03 -0700

    fix syntax error

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,5 +1,5 @@
 class Pizza < ApplicationRecord
-    has_many :pizza_topping, dependent: :destroy
+    has_many :pizza_toppings, dependent: :destroy
     validates :name, length: { maximum: 50 },
                      presence: true,
                      uniqueness: true
